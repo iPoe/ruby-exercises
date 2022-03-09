bc521c772b4f6706cc206811d22001d24ff2b4f3
iPoe <leonardosawi12@gmail.com>
2022-03-09 17:10:37 -0500

    First 3 exercises on vars in ruby

---
+++ ruby_basics/1_data_types/exercises/vars_exercises.rb
@@ -0,0 +1,24 @@
+def greet
+    name = gets.chomp
+    puts "Hello #{name} pleasure to meet you"
+end
+
+def how_old_are_you()
+    puts "How old are you"
+    age = gets.chomp
+    age = age.to_i
+    arr_years = [10, 20, 30, 40]
+    for a in arr_years do
+        puts "In #{a} years you will be: \n#{age+a}"
+    end
+end
+
+def greet_ten_times
+    puts "What is your name?: "
+    name = gets.chomp
+    10.times do
+        puts "Your name is #{name}"
+    end
+end
+
+greet_ten_times
